795cdea35ee24b2b50b2ed53a9def2cb4fe88f43
Sudip Kunda <sudipkunda@gmail.com>
2022-12-03 23:45:30 -0600

    Exercise 1.1

--- porikkha.lua
+++ porikkha.lua
@@ -1,21 +1,18 @@
-function fact (n)
-
-if n == 0 then
-
-  return 1
-
-   else
-
-     return n * fact(n - 1)
-
-      end
-
+function fact(n)
+	if n == 0 then
+		return 1
+	else
+		return n * fact(n-1)
+	end
 end
-
  
 
 print("enter a number:")
 
 a = io.read("*n") -- reads a number
 
+if a < 0 then
+	print("Enter a positive number")
+	os.exit()
+end
 print(fact(a))
