bf088f31d04b64d9099db2b7d2a9928d1bd226ab
Sudip Kunda <sudipkunda@gmail.com>
2022-12-24 01:10:05 -0600

    Try producer consumer programs

--- producer_consumer.lua
+++ producer_consumer.lua
@@ -1,1 +1,15 @@
-print("hum ho")
+-- coroutine example : producer consumer
+-- function producer calls the consumer with data
+function producer()
+	for i = 1,10 do
+		print(i)
+	end
+end
+-- function consumer prints the data
+function consumer()
+	print(1)
+	coroutine.yield()
+	print(2)
+end
+co = coroutine.create(consumer)
+coroutine.resume(co)
